0f1254751d5f4d5044543acf103a7dce7682a91c
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-05-04 10:49:55 +0700

    fix app_offline.htm

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"gopkg.in/fsnotify.v1"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +16,34 @@ func main() {
 			"message": "Hello WOKEP",
 		})
 	})
+
+	// creates a new file watcher for App_offline.htm
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	defer watcher.Close()
+
+	// watch for App_offline.htm and exit the program if present
+	// This allows continuous deployment on App Service as the .exe will not be
+	// terminated otherwise
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if strings.HasSuffix(event.Name, "App_offline.htm") {
+					fmt.Println("Exiting due to App_offline.htm being present")
+					os.Exit(0)
+				}
+			}
+		}
+	}()
+
+	// get the current working directory and watch it
+	currentDir, err := os.Getwd()
+	if err := watcher.Add(currentDir); err != nil {
+		fmt.Println("ERROR", err)
+	}
 
 	port := os.Getenv("HTTP_PLATFORM_PORT")
 
